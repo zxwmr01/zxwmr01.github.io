a3974652226a0cb05d59d7abc2118dfaf7bd0b35
才羽青空 <xiaohuo_6@qq.com>
2026-07-14 23:19:15 +0800

    Update server descriptions for Asia and South America

--- assets/js/serverList.js
+++ assets/js/serverList.js
@@ -4,7 +4,7 @@ var serverList = [
 //name 控制在大厅显示的名称。
         name: "AS SERVER",
 //description 默认不显示，但必须保留该参数。
-        description: "North America Server",
+        description: "Asia Server",
 //ip 控制玩家连接的服务器地址
         ip: "202.189.23.245:24080" 
 
@@ -24,7 +24,7 @@ var serverList = [
 //name 控制在大厅显示的名称。
         name: "SA SERVER",
 //description 默认不显示，但必须保留该参数。
-        description: "European Server",
+        description: "South America Server",
 //ip 控制玩家连接的服务器地址
         ip: "143.14.163.92" 
 
