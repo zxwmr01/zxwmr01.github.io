8cef3addb97398b1f10c16ec69583357392e7071
才羽青空 <xiaohuo_6@qq.com>
2026-08-15 00:51:04 +0800

    Deploy Lobby

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# zxwmr01.github.io
-A lobby for PUBG2017PS Project, made by H4TIUX, lobby based on wtbdev
+# pubg_lobby
+
+### 这是什么？
+
+这是一个尝试完整还原 PUBG 2017 大厅的项目，本项目基于 [OG:BG Lobby](https://git.ogbattlegrounds.com/OGBG/lobby) 修改。
+
+我使用 Trae IDE 设计了大量样式，并对原代码进行了正确格式和缩进还有清理无用代码的操作，使代码更加轻易可读。
+
+
+## 如何使用？
+
+因整个项目为 html 结构文件，您可以直接在本地上复制以 `file：//` 为前缀的 URL 并打开 `OGBATTLEGROUNDS.bat` 文件替换参数 `LobbyUrl=` 后面的 URL 为您刚才复制的内容，然后保存，直接运行就可看到效果。  
+
+我还在为该项目完善更多内容，例如添加缺失的好友列表、服装柜、奖励、活动页面等等内容，并为这些内容添加合适的 CameraAngel（不过我并不知道可以被使用的值）。
+
+
+## 截图预览 
+
+<img src="preview.png" alt="lobby-preview" width="600"> 
+
+特别感谢 OG:BG 团队所有开发者。
---
+++ assets/js/serverList.js
@@ -0,0 +1,32 @@
+var serverList = [
+
+    {
+//name 控制在大厅显示的名称。
+        name: "AS SERVER",
+//description 默认不显示，但必须保留该参数。
+        description: "North America Server",
+//ip 控制玩家连接的服务器地址
+        ip: "202.189.23.245:24080" 
+
+    },
+    
+    {
+//name 控制在大厅显示的名称。
+        name: "EU SERVER",
+//description 默认不显示，但必须保留该参数。
+        description: "European Server",
+//ip 控制玩家连接的服务器地址
+        ip: "stupidpieceofshit.space:8888" 
+
+    },
+
+    {
+//name 控制在大厅显示的名称。
+        name: "SA SERVER",
+//description 默认不显示，但必须保留该参数。
+        description: "European Server",
+//ip 控制玩家连接的服务器地址
+        ip: "107.149.153.36:8888" 
+
+    }
+]
